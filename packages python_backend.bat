e5df5a07af2546b38b6f30381ff46b2878b97c16
Fabio Krämer <fabio.kraemer@freenet.de>
2019-06-13 22:36:29 +0200

    Integrationstest, .bat Datei  für das Laden der benötigten Packages

---
+++ packages python_backend.bat
@@ -0,0 +1,11 @@
+echo Load packages for SUMZ python-backend
+
+pip install flask
+pip install numpy
+pip install statsmodels
+pip install scipy==1.2 --upgrade
+pip install pmdarima
+pip install jsonschema
+pip install jsonref
+
+echo finished
